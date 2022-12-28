9bce57f7d8eb41a45e75c67c9516ca8d943917ba
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-28 21:00:37 +0530

    Program 5 Factors of number.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 
-
-number = int(input("Enter the Harmonic Number : "))
+number = int(input("Enter a number the Prime Number : "))
 number_range = range(number)
-harmonic_number = 0
 
-if number == 0:
-        print("Enter the valid Harmonic Number..!")
-else:
-        for i in number_range:
-                i += 1
-                harmonic_number += (1/i)
-        print("Harmonic number for ", number, " is : ", round(harmonic_number, 2), type(harmonic_number))
+for i in number_range:
+        i += 2
+        while number % i == 0:
+                        print(i, " ")
+                        number /= i
+if number > 2:
+    print(number)
