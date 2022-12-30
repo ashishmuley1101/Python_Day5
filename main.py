fe628f24f587e1c3e7ad763ba851e18377311cd6
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-30 23:28:23 +0530

    Program 10 to Find the Largest Among Three Numbers.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 
-char = input("Enter the Alphabet : ")
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second numbers : "))
+num3 = int(input("Enter the third numbers : "))
 
-if char == 'a':
-    print(char + " is vowel.")
-elif char == 'e':
-    print(char + " is vowel.")
-elif char == 'i':
-    print(char + " is vowel.")
-elif char == 'o':
-    print(char + " is vowel.")
-elif char == 'u':
-    print(char + " is vowel.")
+if num1 > num2 and num1 > num3:
+    print("The largest number is : ",num1)
+elif num2 > num1 and num2 > num3:
+    print("The largest number is : ", num2)
+elif num3 > num1 and num3 > num2:
+    print("The largest number is : ", num3)
 else:
-    print(char + " is consonant.")
+    print("The numbers are same.")
 
 
 
