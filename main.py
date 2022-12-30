882e78c4d8e29f10cb4b88d5e9bebdd6d3690f76
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-30 20:12:20 +0530

    Program 6 Compute Quotient and Remainder.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 
 
-number = int(input("Enter the Harmonic Number : "))
-number_range = range(number)
-harmonic_number = 0
+divident = int(input("Enter the value for Dividend : "))
+divisor = int(input("Enter the value for Divisor : "))
 
-if number == 0:
-        print("Enter the valid Harmonic Number..!")
-else:
-        for i in number_range:
-                i += 1
-                harmonic_number += (1/i)
-        print("Harmonic number for ", number, " is : ", round(harmonic_number, 2), type(harmonic_number))
+quotient = divident / divisor
+remainder = divident % divisor
+
+print("Quotient for your given valve : ", quotient)
+print("Reaminder for your given valve : ", remainder)
