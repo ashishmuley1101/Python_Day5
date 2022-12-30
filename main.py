12360c0d879d5a7019ccd88cf90f910bd9e745e3
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-30 23:02:26 +0530

    Program 8 to Check Whether a Number is Even or Odd.

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 
-first_num = int(input("Enter the First Number : "))
-second_num = int(input("Enter the Second Number : "))
+number = int(input("Enter any Number : "))
 
-print("Before swapping Numbers are : ", first_num,"  ", second_num)
+if number % 2 == 0:
+    print(number, "is even number.")
+else:
+    print(number, " is odd number.")
 
-swap_num = first_num
-first_num = second_num
-second_num = swap_num
 
-print("After swapping Numbers are : ", first_num,"  ", second_num)
 
